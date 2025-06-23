3465e1dd8fea42b364ca82d4db9fb872d581e347
anish155 <anishtandukar3@gmail.com>
2025-06-23 20:46:13 +0545

    Basic about page ready

---
+++ src/component/about.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+function About() {
+    return(
+        <div>
+            <h1 className="header">About Us</h1>
+            <p className="box">
+                This is a dedicated web for Super M a local maket located at ..... location.<br/>
+                It serves as a positive work environment for both the locals and the visitors.
+            </p>
+            <Link to="/">Home Page</Link>
+        </div>
+    );
+}
+export default About
