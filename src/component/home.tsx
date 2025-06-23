7380fa19583d60b94cb9bb71b3e6926f3bd30306
anish155 <anishtandukar3@gmail.com>
2025-06-23 20:46:56 +0545

    Basic home page ready

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Home(){
     return(
         <div>
             <h1>Super M</h1>
             <p>This is a super market website.</p>
+            <Link to="/about">About Page</Link>
         </div>
     );
 }
