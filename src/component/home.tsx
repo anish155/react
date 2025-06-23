b37d33bbe840cbc3aacc95dd671aa307d19a606a
anish155 <anishtandukar3@gmail.com>
2025-06-23 20:16:23 +0545

    workspace for react

---
+++ src/component/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+            <h1>Super M</h1>
+            <p>This is a super market website.</p>
+        </div>
+    );
+}
+
+export default Home;
