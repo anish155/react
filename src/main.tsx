bfd6bd5256ee088fa5fe0a2a8d2f1ea8100664e7
anish155 <anishtandukar3@gmail.com>
2025-06-23 20:47:49 +0545

    Main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 // import App from './App.tsx'
 import Home from './component/home.tsx'
+import About from './component/about.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-    // <App />
-  // </StrictMode>
-  <Home />
+  <BrowserRouter>
+  <Routes>
+  <Route path='/' element={<Home />}/>
+  <Route path='/about' element={<About />}/>
+  </Routes>
+  </BrowserRouter>
 )
