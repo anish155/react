775e77b3cf2d36ba610fb74e74491017accddf1a
anish155 <anishtandukar3@gmail.com>
2025-06-24 21:34:35 +0545

    adding nav bar to home

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -1,11 +1,15 @@
 // import React from "react";
+import './css/home.css';
 import { Link } from "react-router-dom";
 function Home(){
     return(
-        <div>
+        <div className='home'>
+            <nav><Link to="/">Home</Link>
+            &nbsp;
+            <Link to="/about">About</Link>
+            </nav>
             <h1>Super M</h1>
             <p>This is a super market website.</p>
-            <Link to="/about">About Page</Link>
         </div>
     );
 }
