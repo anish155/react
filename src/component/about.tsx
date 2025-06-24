91b74b79911ab8489774a943729fe81a4307f710
anish155 <anishtandukar3@gmail.com>
2025-06-24 21:33:55 +0545

    adding navbar to about

--- src/component/about.tsx
+++ src/component/about.tsx
@@ -1,14 +1,18 @@
 // import React from "react";
+import './css/about.css';
 import {Link} from 'react-router-dom';
 function About() {
     return(
-        <div>
+        <div className='about'>
+            <nav><Link to="/">Home</Link>
+            &nbsp;
+            <Link to="/about">About</Link>
+            </nav>
             <h1 className="header">About Us</h1>
             <p className="box">
                 This is a dedicated web for Super M a local maket located at ..... location.<br/>
                 It serves as a positive work environment for both the locals and the visitors.
             </p>
-            <Link to="/">Home Page</Link>
         </div>
     );
 }
